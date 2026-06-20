20416819bb309f4fc001c582c548e1fc064ab817
Valentin039 <valentin.ojeda039@gmail.com>
2026-06-19 21:00:43 -0300

    creación config.txt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] lineas = File.ReadAllLines("config.txt");
+
+int puerto = 0;
+string carpeta = "";
+
+foreach (string linea in lineas)
+{
+    string[] partes = linea.Split('=');
+    if (partes[0] == "PORT")
+    {
+        puerto = int.Parse(partes[1]);
+    }
+    else if (partes[0] == "ROOT")
+    {
+        carpeta = partes[1];
+    }
+}
